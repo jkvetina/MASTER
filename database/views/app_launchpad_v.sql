525f5be28a68f3387aba2f907cbf3be385b84477
Jan Kvetina <jan.kvetina@gmail.com>
2023-09-12 22:11:58 +0200

    Show only assigned apps

--- database/views/app_launchpad_v.sql
+++ database/views/app_launchpad_v.sql
@@ -1,30 +1,42 @@
 CREATE OR REPLACE FORCE VIEW app_launchpad_v AS
+WITH x AS (
+    SELECT /*+ MATERIALIZE */
+        u.app_id
+    FROM app_users_map u
+    JOIN app_applications a
+        ON a.app_id         = u.app_id
+        AND a.is_active     = 'Y'
+    WHERE u.user_id         = core.get_user_id()
+        AND u.is_active     = 'Y'
+)
 SELECT
-    a.workspace,
-    a.app_id,
-    a.app_alias,
-    a.app_prefix,
-    a.app_name,
-    a.app_desc,
+    m.workspace,
+    m.app_id,
+    m.app_alias,
+    m.app_prefix,
+    m.app_name,
+    m.app_desc,
     --
     APEX_PAGE.GET_URL (
         p_session       => core.get_session_id(),
-        p_application   => a.app_id,
+        p_application   => m.app_id,
         p_page          => 'HOME'           -- get real homepage
     ) AS app_link
     --
-FROM app_navigation_map_mv a
-WHERE a.app_group       = 'LAUNCHPAD'
+FROM app_navigation_map_mv m
+JOIN x
+    ON x.app_id         = m.app_id
+WHERE m.app_group       = 'LAUNCHPAD'
 GROUP BY
-    a.workspace,
-    a.app_id,
-    a.app_alias,
-    a.app_prefix,
-    a.app_name,
-    a.app_desc
+    m.workspace,
+    m.app_id,
+    m.app_alias,
+    m.app_prefix,
+    m.app_name,
+    m.app_desc
 ORDER BY
-    a.workspace,
-    a.app_name;
+    m.workspace,
+    m.app_name;
 --
 COMMENT ON TABLE app_launchpad_v IS '';
 
