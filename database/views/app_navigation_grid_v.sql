4a0f4cf53cf01d85c2478338d0e4a2bbe648ea97
Jan Kvetina <jan.kvetina@gmail.com>
2023-09-18 23:38:06 +0200

    Show add/remove pages (and before MV refresh)

--- database/views/app_navigation_grid_v.sql
+++ database/views/app_navigation_grid_v.sql
@@ -3,7 +3,21 @@ WITH x AS (
     SELECT /*+ MATERIALIZE */
         core.get_app_id()       AS app_id
     FROM DUAL
+),
+live_pages AS (
+    SELECT /*+ MATERIALIZE */
+        p.application_id        AS app_id,
+        p.page_id,
+        p.page_alias,
+        p.page_group,
+        p.page_name,
+        p.page_mode,
+        p.authorization_scheme
+    FROM x
+    JOIN apex_application_pages p
+        ON p.application_id     = x.app_id
 )
+-- existing pages
 SELECT
     NVL(m.app_id, n.app_id)     AS app_id,
     NVL(m.page_id, n.page_id)   AS page_id,
@@ -61,20 +75,102 @@ SELECT
     --
     NVL(z.label__, REPLACE(RPAD(' ', 4, ' '), ' ', '&' || 'nbsp; ') || m.page_name) AS label,
     --
-    NVL(z.order#, f.order# || '/' || LTRIM(n.col_id || '.' || n.order#, '.') || '.' || n.page_id) AS sort_order#
+    z.order# AS sort_order#
     --
 FROM app_navigation_map_mv m
 JOIN x
     ON x.app_id         = m.app_id
-LEFT JOIN app_navigation n
+JOIN app_navigation n
     ON n.app_id         = m.app_id
     AND n.page_id       = m.page_id
 LEFT JOIN app_navigation_v z
-    ON z.app_id         = m.app_id
-    AND z.page_id       = m.page_id
-LEFT JOIN app_navigation_v f
-    ON f.app_id         = n.app_id
-    AND f.page_id       = n.parent_id;
+    ON z.app_id         = n.app_id
+    AND z.page_id       = n.page_id
+--
+-- remove pages
+UNION ALL
+SELECT
+    n.app_id,
+    n.page_id,
+    --
+    core.get_icon('fa-trash-o') AS action_name,
+    core.get_page_url (
+        in_page_id  => 850,
+        in_names    => 'P850_REMOVE_PAGE',
+        in_values   => n.page_id
+    ) AS action_link,
+    --
+    'D' AS action_dml,
+    --
+    n.parent_id,
+    n.order#,
+    n.col_id,
+    n.is_hidden,
+    n.is_reset,
+    --
+    NULL AS page_alias,
+    NULL AS page_name,
+    ' '  AS page_group,     -- to show first
+    NULL AS auth_scheme,
+    NULL AS page_link,
+    NULL AS is_modal,
+    NULL AS css_classes,
+    NULL AS label,
+    --
+    NULL AS sort_order#
+    --
+FROM app_navigation n
+JOIN x
+    ON x.app_id         = n.app_id
+LEFT JOIN live_pages p
+    ON p.app_id         = n.app_id
+    AND p.page_id       = n.page_id
+WHERE p.page_id         IS NULL
+--
+-- add pages
+UNION ALL
+SELECT
+    p.app_id,
+    p.page_id,
+    --
+    core.get_icon('fa-plus') AS action_name,
+    core.get_page_url (
+        in_page_id  => 850,
+        in_names    => 'P850_ADD_PAGE',
+        in_values   => p.page_id
+    ) AS action_link,
+    --
+    ''   AS action_dml,
+    --
+    NULL AS parent_id,      -- guess ?
+    NULL AS order#,         -- guess
+    NULL AS col_id,
+    --
+    CASE WHEN p.page_mode != 'Normal' THEN 'Y' END AS is_hidden,
+    'Y' AS is_reset,
+    --
+    p.page_alias,
+    p.page_name,
+    p.page_group,
+    p.authorization_scheme,
+    --
+    core.get_page_url (
+        in_app_id       => p.app_id,
+        in_page_id      => p.page_id
+    ) AS page_link,
+    --
+    CASE WHEN p.page_mode != 'Normal' THEN 'Y' END AS is_modal,
+    NULL AS css_classes,
+    --
+    p.page_name AS label,
+    --
+    NULL AS sort_order#
+    --
+FROM live_pages p
+LEFT JOIN app_navigation n
+    ON n.app_id         = p.app_id
+    AND n.page_id       = p.page_id
+WHERE n.page_id         IS NULL;
 --
 COMMENT ON TABLE app_navigation_grid_v IS '';
 
