93e60d2e91220624cd7fff8550b605844bc7e59c
Jan Kvetina <jan.kvetina@gmail.com>
2023-09-29 15:58:25 +0200

    Seed data

--- patches/60_data/app_navigation.sql
+++ patches/60_data/app_navigation.sql
@@ -10,8 +10,12 @@ WHERE app_id = 800;
 --
 MERGE INTO app_navigation t
 USING (
-    SELECT 800 AS app_id, 0 AS page_id, NULL AS parent_id, NULL AS is_hidden, NULL AS is_reset, 666 AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 0 AS page_id, NULL AS parent_id, NULL AS is_hidden, NULL AS is_reset, NULL AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 100 AS page_id, NULL AS parent_id, NULL AS is_hidden, NULL AS is_reset, 0 AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 700 AS page_id, NULL AS parent_id, 'Y' AS is_hidden, 'Y' AS is_reset, 820 AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 705 AS page_id, 700 AS parent_id, 'Y' AS is_hidden, 'Y' AS is_reset, 10 AS order#, 1 AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 750 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, 4 AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 770 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 25 AS order#, 4 AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 800 AS page_id, NULL AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 800 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 810 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, 2 AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 815 AS page_id, 810 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, NULL AS col_id FROM DUAL UNION ALL
@@ -20,18 +24,21 @@ USING (
     SELECT 800 AS app_id, 830 AS page_id, 820 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 15 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 835 AS page_id, 820 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 20 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 840 AS page_id, 820 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 25 AS order#, NULL AS col_id FROM DUAL UNION ALL
-    SELECT 800 AS app_id, 850 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 15 AS order#, 1 AS col_id FROM DUAL UNION ALL
-    SELECT 800 AS app_id, 860 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 20 AS order#, 3 AS col_id FROM DUAL UNION ALL
-    SELECT 800 AS app_id, 862 AS page_id, 860 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 20 AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 850 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 15 AS order#, 4 AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 860 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 15 AS order#, 3 AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 862 AS page_id, 860 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 865 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, 3 AS col_id FROM DUAL UNION ALL
-    SELECT 800 AS app_id, 870 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 30 AS order#, 3 AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 870 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 20 AS order#, 4 AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 880 AS page_id, 800 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, 1 AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 885 AS page_id, 880 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 10 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 888 AS page_id, 880 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 15 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 890 AS page_id, 880 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 20 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 895 AS page_id, 880 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 25 AS order#, NULL AS col_id FROM DUAL UNION ALL
     SELECT 800 AS app_id, 900 AS page_id, NULL AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 990 AS order#, NULL AS col_id FROM DUAL UNION ALL
-    SELECT 800 AS app_id, 980 AS page_id, NULL AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 980 AS order#, NULL AS col_id FROM DUAL
+    SELECT 800 AS app_id, 980 AS page_id, NULL AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 980 AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 985 AS page_id, 980 AS parent_id, 'Y' AS is_hidden, 'Y' AS is_reset, NULL AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 986 AS page_id, 880 AS parent_id, NULL AS is_hidden, 'Y' AS is_reset, 30 AS order#, NULL AS col_id FROM DUAL UNION ALL
+    SELECT 800 AS app_id, 9999 AS page_id, 900 AS parent_id, 'Y' AS is_hidden, NULL AS is_reset, NULL AS order#, NULL AS col_id FROM DUAL
 ) s
 ON (
     t.app_id = s.app_id
