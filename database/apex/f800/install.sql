8d40c1544a74d087c3d128173abaf7a0afd3bceb
Jan Kvetina <jan.kvetina@gmail.com>
2023-09-30 15:32:15 +0200

    Fix tabs to reflect color on Launchpad

--- database/apex/f800/install.sql
+++ database/apex/f800/install.sql
@@ -10,8 +10,8 @@ prompt --install
 @@application/shared_components/files/icons_app_icon_144_rounded_png.sql
 @@application/shared_components/files/icons_app_icon_256_rounded_png.sql
 @@application/shared_components/files/app_css.sql
-@@application/shared_components/files/app_min_css.sql
 @@application/shared_components/files/pwa_master_screen_launchpad_png.sql
+@@application/shared_components/files/app_min_css.sql
 @@application/shared_components/files/icons_app_icon_32_png.sql
 @@application/shared_components/files/icons_app_icon_192_png.sql
 @@application/shared_components/files/icons_app_icon_512_png.sql
