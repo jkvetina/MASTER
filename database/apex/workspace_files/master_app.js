02f2db12186eabfb4be56e05ac003f8b4293720c
Jan Kvetina <jan.kvetina@gmail.com>
2023-11-02 20:48:21 +0100

    Fix message hooks

--- database/apex/workspace_files/master_app.js
+++ database/apex/workspace_files/master_app.js
@@ -5,7 +5,7 @@ var ping_active = true;
 var ping_loop;
 var last_scheduler;
 //
-const init_page = function() {
+const init_page_asap = function() {
     const autohide_after = 2300;
 
     // autohide success messages
@@ -118,7 +118,9 @@ const init_page = function() {
     if (ping_active && ping_interval > 0 && apex.item('P0_AJAX_PING_INTERVAL').node) {
         ping_loop = ping_fn();
     }
-
+};
+//
+const init_page = function() {
     //
     // ADJUST GRIDS
     //
@@ -145,6 +147,7 @@ $('button > .t-Button-label').each(function(k, id) {
 
 // when page is loaded
 $(function() {
+    init_page_asap();
 });
 
 // when all APEX components are loaded
@@ -621,7 +624,7 @@ const show_action_menu = function(e) {
     //
     var $id = $(this).attr('id');  // e.target.id, this.triggeringElement.id;
     var pos = $('button#' + $id).offset();
-    console.log('BUTTON', $id, pos, 'MENU', $('div.ACTION_MENU').offset());
+    console.log('BUTTON', $id, pos, 'MENU', $('div.ACTION_MENU').offset(), $('div.ACTION_MENU[data-id="' + $id + '"]'));
     //
     $('div.ACTION_MENU').css({
         display   : 'none'
