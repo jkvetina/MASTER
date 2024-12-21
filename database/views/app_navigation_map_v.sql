7c9cb20bc82cc27d8617f324d49d4f8e848fe512
Jan Kvetina <jan.kvetina@gmail.com>
2024-12-21 07:51:30 +0100

    A wrapped above navigation MV

--- database/views/app_navigation_map_v.sql
+++ database/views/app_navigation_map_v.sql
@@ -1,81 +1,56 @@
 CREATE OR REPLACE FORCE VIEW app_navigation_map_v AS
-WITH n AS (
-    -- pull data from Navigation table, where tree structure is maintained
-    SELECT /*+ MATERIALIZE */
-        n.app_id,
-        n.target_app_id,
-        n.target_page_id,
-        n.parent_id,
-        n.is_hidden,
-        n.is_reset,
-        n.order#,
-        n.col_id
-    FROM app_navigation n
-),
-t AS (
-    -- pull materialized data and convert empty columns back to NULLs
-    SELECT /*+ MATERIALIZE */
-        t.workspace,
-        t.app_id,
-        t.app_alias,
-        t.app_owner,
-        t.app_group,
-        t.app_name,
-        t.app_version,
-        t.app_auth,
-        NULLIF(t.app_desc, '-')         AS app_desc,            -- convert back to real NULL values
-        --
-        t.page_id,
-        t.page_name,
-        NULLIF(t.page_alias, '-')       AS page_alias,
-        NULLIF(t.page_title, '-')       AS page_title,
-        NULLIF(t.page_group, '-')       AS page_group,
-        NULLIF(t.page_css_classes, '-') AS page_css_classes,
-        t.page_mode,
-        t.page_template,
-        --
-        NULLIF(t.auth_scheme, '-')      AS auth_scheme,
-        t.home_link
-        --
-    FROM app_navigation_map_mv t
-)
 SELECT
-    -- construct a tree structure for the navigation
+    --
+    -- to recover NULL values forced by using MV
+    -- and to calculate proper page name
+    --
     n.app_id,
     n.target_app_id,
     n.target_page_id,
     --
-    TO_NUMBER(REGEXP_SUBSTR(LTRIM(SYS_CONNECT_BY_PATH(n.parent_id, '/'), '/'), '\d+')) AS page_root_id,
+    NULLIF(n.page_root_id, 0)   AS page_root_id,
+    NULLIF(n.parent_id, 0)      AS parent_id,
+    NULLIF(n.is_hidden, '-')    AS is_hidden,
+    NULLIF(n.is_reset, '-')     AS is_reset,
     --
-    UPPER(REPLACE(APEX_STRING_UTIL.GET_SLUG(t.page_group), '-', '_')) AS page_group,
+    n.workspace,
+    n.app_alias,
+    n.app_owner,
+    n.app_group,
+    n.app_name,
+    n.app_version,
+    n.app_auth,
     --
-    core.get_page_name(in_name => t.page_name) AS page_label,  -- to support #icons
+    NULLIF(n.app_desc, '-')     AS app_desc,
+    NULLIF(n.app_prefix, '-')   AS app_prefix,
     --
-    t.page_alias,
-    t.page_css_classes,
-    t.auth_scheme,
-    t.app_auth,
-    t.app_alias,
+    NULLIF(n.page_alias, '-')   AS page_alias,
+    n.page_name,
     --
-    n.parent_id,
-    n.is_hidden,
-    n.is_reset,
+    core.get_page_name (
+        in_page_id      => n.target_page_id,
+        in_app_id       => n.target_app_id,
+        in_name         => n.page_name,
+        in_replace      => 'Y'
+    ) AS page_label,
     --
-    LEVEL               AS depth,
-    LEAST(LEVEL, 2)     AS lvl,
+    NULLIF(n.page_group, '-')           AS page_group,
+    NULLIF(n.page_group_slug, '_')      AS page_group_slug,     -- not a dash
+    NULLIF(n.page_css_classes, '-')     AS page_css_classes,
     --
-    COALESCE(n.col_id, PRIOR n.col_id, 0) AS col_id,
+    n.page_mode,
+    n.page_template,
     --
-    SYS_CONNECT_BY_PATH(n.target_page_id, '/') || '/' AS pages_path,
+    NULLIF(n.auth_scheme, '-')  AS auth_scheme,
+    NULLIF(n.col_id, 0)         AS col_id,
     --
-    SYS_CONNECT_BY_PATH(NVL(n.col_id, 0) || '.' || n.order# || '.' || n.target_page_id, '/') AS order#
+    n.depth,
+    n.is_leaf,
+    n.lvl,
+    n.lvl_indent,
+    n.pages_path,
+    n.order#
     --
-FROM t
-JOIN n
-    ON n.target_app_id      = t.app_id
-    AND n.target_page_id    = t.page_id
-CONNECT BY n.app_id         = PRIOR n.target_app_id
-    AND n.parent_id         = PRIOR n.target_page_id
-START WITH n.parent_id      IS NULL;
+FROM app_navigation_map_mv n;
 /
 
